c2380a6f4e7bef70008aeb698eeec0bcf3a21952
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-02-10 13:34:14 -0300

    feature(MSSPRING) - initial project

---
+++ src/main/java/dev/cryss/eurekaservernameserving/EurekaserverNameServingApplication.java
@@ -0,0 +1,15 @@
+package dev.cryss.eurekaservernameserving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaserverNameServingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverNameServingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cryss/eurekaservernameserving/EurekaserverNameServingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cryss.eurekaservernameserving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverNameServingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
